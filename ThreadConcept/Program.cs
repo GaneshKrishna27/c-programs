a994dc864960aa8d941346921ece3d6c07e5f01b
GaneshKrishna27 <gancheeriganesh@gmail.com>
2020-01-10 17:22:01 +0530

    My fourth

--- ThreadConcept/Program.cs
+++ ThreadConcept/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadConcept
 {
@@ -10,6 +11,33 @@ namespace ThreadConcept
     {
         static void Main(string[] args)
         {
+            ThreadClass tc = new ThreadClass();
+            Thread th = new Thread(tc.countEven);
+            Thread th1 = new Thread(tc.countOdd);
+            th.Start();
+            th1.Start();
+            Console.WriteLine("main thread ends");
+            Console.ReadLine();
         }
     }
+    class ThreadClass
+    {
+        public void countEven()
+        {
+            for(int i=0;i<100;i+=2)
+            {
+                Console.WriteLine(i);
+                if (i == 60) Thread.Sleep(4000);
+
+            }
+        }
+        public void countOdd()
+        {
+            for(int i =1;i<100;i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
 }
